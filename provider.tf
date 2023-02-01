1743828cfbdaf6ed526d466bec589b15f0b5983d
Mike Babs <mikeybabs40@gmail.com>
2023-02-01 17:13:26 +0000

    Changd provider details

--- provider.tf
+++ provider.tf
@@ -1,9 +1,20 @@
+# variable "instance_config" {
+#   type = map(any)
+# }
+
+
+terraform {
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.51.0"
+    }
+  }
+}
+
+
 provider "aws" {
   region  = "eu-west-1"
   profile = "default"
 }
-
-
-# variable "instance_config" {
-#   type = map(any)
-# }
